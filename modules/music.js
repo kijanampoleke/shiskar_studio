22fbbcf77806f52c555125fd5099a1cb6c3a9214
kijanampoleke <kijanampole676@gmail.com>
2025-10-27 14:15:34 +0300

    Delete modules directory

--- modules/music.js
+++
@@ -1,9 +0,0 @@
-export function renderMusic() {
-  const section = document.getElementById("music");
-  section.innerHTML = `
-    <h2>Music Manager</h2>
-    <iframe src="https://audiomack.com/embed/..." width="100%" height="120"></iframe>
-    <iframe src="https://w.soundcloud.com/player/..." width="100%" height="120"></iframe>
-    <p>Spotify integration and release toggles coming soon.</p>
-  `;
-}
